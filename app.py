f57ecdf1f81f1d99c4f94d820e4d98c142ee2e4d
Huuwx <chumanhhuu2@gmail.com>
2024-09-24 22:41:54 +0700

    initial

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+
+app = Flask(__name__)
+
+# Tải các mô hình
+models = {}
+model_names = ['Linear', 'Lasso', 'NeuralNetwork', 'Stacking']
+for model_name in model_names:
+    with open(f'Model/{model_name}Model', 'rb') as file:
+        models[model_name] = pickle.load(file)
+
+# Khởi tạo scaler và lấy giá trị mean và std từ mô hình đã huấn luyện
+# with open('scaler.pkl', 'rb') as file:
+#     scaler = pickle.load(file)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
+    if request.method == 'POST':
+        squareMeters = float(request.form['squareMeters'])
+        numbeOfRoom = float(request.form['numbeOfRoom'])
+        hasYard = float(request.form['hasYard'])
+        hasPool = float(request.form['hasPool'])
+        floors = float(request.form['floors'])
+        cityCode = float(request.form['cityCode'])
+        cityPartRange = float(request.form['cityPartRange'])
+        numPrevOwners = float(request.form['numPrevOwners'])
+        made = float(request.form['made'])
+        isNewBuilt = float(request.form['isNewBuilt'])
+        hasStormProtector = float(request.form['hasStormProtector'])
+        basement = float(request.form['basement'])
+        attic = float(request.form['attic'])
+        garage = float(request.form['garage'])
+        hasStorageRoom = float(request.form['hasStorageRoom'])
+        hasGuestRoom = float(request.form['hasGuestRoom'])
+        selected_model = request.form['model']
+
+        input_data = np.array([[squareMeters, numbeOfRoom, hasYard, hasPool, floors, cityCode, cityPartRange, numPrevOwners, made, isNewBuilt, hasStormProtector, basement, attic, garage, hasStorageRoom, hasGuestRoom]])
+
+        # Dự đoán với mô hình đã chọn
+        prediction = models[selected_model].predict(input_data)[0]
+
+
+    return render_template('index.html', prediction=prediction, model_names=model_names)
+
+if __name__ == '__main__':
+    app.run(debug=True)
